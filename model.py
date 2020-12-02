fa5d251f0f5ccd47ab91be4029870d4bd5a47aef
MoriSama <A-LittleSeven@users.noreply.github.com>
2020-12-02 10:16:28 +0800

    add docString, use compat.v1 surpress warning

--- model.py
+++ model.py
@@ -1,54 +1,117 @@
 import tensorflow as tf
 
 
-def leNet(imgs, num_classes, training=True):
-    # input_layer = tf.reshape(imgs, [-1, 28, 28, 1], name="input_layer")
+def LeNet(imgs, n_classes, mode):
+    """Model function for CNN."""
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    # MNIST images are 28x28 pixels, and have one color channel
+    input_layer = tf.reshape(imgs, [-1, 28, 28, 1], name="input_layer")
+    tf.compat.v1.summary.image("input_layer", input_layer)
 
-    conv_1 = tf.layers.conv2d(inputs=imgs,
-                              filters=32,
-                              kernel_size=[5, 5],
-                              padding="same",
-                              activation=tf.nn.relu)
+    # Convolutional Layer #1
+    # Computes 32 features using a 5x5 filter with ReLU activation.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 28, 28, 1]
+    # Output Tensor Shape: [batch_size, 28, 28, 32]
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
 
-    pool_1 = tf.layers.max_pooling2d(inputs=conv_1, pool_size=[2, 2], strides=2)
+    # Pooling Layer #1
+    # First max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 28, 28, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 32]
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-    conv_2 = tf.layers.conv2d(inputs=pool_1,
-                              filters=64,
-                              kernel_size=[5, 5],
-                              padding="same",
-                              activation=tf.nn.relu)
+    # Convolutional Layer #2
+    # Computes 64 features using a 5x5 filter.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 14, 14, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 64]
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
 
-    pool_2 = tf.layers.max_pooling2d(inputs=conv_2, pool_size=[2, 2], strides=2)
+    # Pooling Layer #2
+    # Second max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 14, 14, 64]
+    # Output Tensor Shape: [batch_size, 7, 7, 64]
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-    flatten = tf.reshape(pool_2, [-1, 7 * 7 * 64])
+    # Flatten tensor into a batch of vectors
+    # Input Tensor Shape: [batch_size, 7, 7, 64]
+    # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
-    dense = tf.layers.dense(inputs=flatten, units=1024, activation=tf.nn.relu)
+    # Dense Layer
+    # Densely connected layer with 1024 neurons
+    # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+    # Output Tensor Shape: [batch_size, 1024]
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
-    dropout = tf.layers.dropout(inputs=dense, rate=0.5, training=training)
+    # Add dropout operation; 0.6 probability that element will be kept
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode)
 
-    softmax = tf.layers.dense(inputs=dropout, units=num_classes, name="softmax_linear")
-    return softmax
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 10]
+    logits = tf.layers.dense(inputs=dropout, units=n_classes, name="softmax_linear")
+    return logits
 
 
-def losses(softmax, labels):
-    with tf.variable_scope("loss") as scope:
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=softmax, labels=labels)
-        loss = tf.reduce_mean(cross_entropy, name="loss")
-        tf.summary.scalar(scope.name + "/loss", loss)
+def losses(logits, labels):
+    """
+    Compute loss from logits and labels
+    Args:
+        logits: logits tensor, float, [batch_size, n_classes]
+        labels: label tensor, tf.int32, [batch_size]
+    Returns:
+        loss tensor of float type
+    """
+    with tf.compat.v1.variable_scope('loss') as scope:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits \
+            (logits=logits, labels=labels, name='xentropy_per_example')
+        loss = tf.reduce_mean(cross_entropy, name='loss')
+        tf.compat.v1.summary.scalar(scope.name + '/loss', loss)
     return loss
 
 
-def training(loss, lr, global_step):
-    with tf.name_scope("optimizer"):
-        opt = tf.train.AdamOptimizer(learning_rate=lr)
-        train_op = opt.minimize(loss, global_step=global_step)
+def training(loss, learning_rate, global_step):
+    """
+    Training ops, the Op returned by this function is what must be passed to
+        'sess.run()' call to cause the model to train.
+    Args:
+        loss: loss tensor, from losses()
+    Returns:
+        train_op: The op for trainning
+    """
+    with tf.name_scope('optimizer'):
+        train_op = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, global_step=global_step)
     return train_op
 
 
-def evaluate(softmax, labels):
-    with tf.variable_scope("accuracy") as scope:
-        correct = tf.nn.in_top_k(softmax, labels, 1)
+def evaluation(logits, labels):
+    """
+    Evaluate the quality of the logits at predicting the label.
+    Args:
+        logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+        labels: Labels tensor, int32 - [batch_size], with values in the
+        range [0, NUM_CLASSES).
+    Returns:
+        A scalar int32 tensor with the number of examples (out of batch_size)
+        that were predicted correctly.
+    """
+    with tf.compat.v1.variable_scope('accuracy') as scope:
+        correct = tf.nn.in_top_k(logits, labels, 1)
         correct = tf.cast(correct, tf.float16)
         accuracy = tf.reduce_mean(correct)
-        tf.summary.scalar(scope.name + "/accuracy", accuracy)
+        tf.compat.v1.summary.scalar(scope.name + '/accuracy', accuracy)
     return accuracy
